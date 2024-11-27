cc18007d810a328220b4ac5d11564e081dbc05e9
원민관 <mkwan5741@gmail.com>
2024-11-27 14:56:31 +0900

    Routing Configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import {
+  LandingPage,
+  HomePage,
+  QuestionListPage,
+  TagListPage,
+  MyPage,
+  PhoneVerificationPage,
+  NotFoundPage,
+  TermsAndPrivacyPage,
+  QuestionEditPage,
+} from "./pages";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/question" element={<QuestionListPage />} />
+      <Route path="/tag" element={<TagListPage />} />
+      <Route path="/my" element={<MyPage />} />
+      <Route path="/phone" element={<PhoneVerificationPage />} />
+      <Route path="/not-found" element={<NotFoundPage />} />
+      <Route path="/privacy" element={<TermsAndPrivacyPage />} />
+      <Route path="/edit" element={<QuestionEditPage />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
