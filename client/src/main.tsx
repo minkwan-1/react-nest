46b2dc54a0240247ec3839459441b8ea2e7647d4
원민관 <mkwan5741@gmail.com>
2024-11-28 18:26:06 +0900

    ThemeProvider setting

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./reset.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import ThemeProviderWrapper from "./providers/ThemeProviderWrapper.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProviderWrapper>
+        <App />
+      </ThemeProviderWrapper>
     </BrowserRouter>
   </StrictMode>
 );
