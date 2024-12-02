aea0dc9bdf5fee40de43a4c4d43b0fd18762f922
원민관 <mkwan5741@gmail.com>
2024-12-02 11:57:34 +0900

    ScrollToTop component, in Router Provider

--- client/src/providers/RouterProvider.tsx
+++ client/src/providers/RouterProvider.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-
+import ScrollToTop from "../components/layout/common/ScrollToTop";
 const RouterProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  return <BrowserRouter>{children}</BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      {children}
+    </BrowserRouter>
+  );
 };
 
 export default RouterProvider;
