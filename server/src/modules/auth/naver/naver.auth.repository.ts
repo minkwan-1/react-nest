e1b6812573fe55778317c762b1e4c574e56387fb
원민관 <mkwan5741@gmail.com>
2025-03-19 20:59:12 +0900

    깃허브 테스트3

--- server/src/modules/auth/naver/naver.auth.repository.ts
+++ server/src/modules/auth/naver/naver.auth.repository.ts
@@ -12,7 +12,7 @@ export class NaverAuthRepository {
 
   // 1. 사용자 정보가 존재하는지 확인하고, 없으면 생성
   async findUser(user: { id: string }): Promise<NaverUser> {
-    console.log('깃허브 테스트용22222s:', user);
+    console.log('깃허브 테스트용3333:', user);
     const existingUser = await this.naverUserRepository.findOneBy({
       id: user?.id,
     });
