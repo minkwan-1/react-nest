2d40a9a6c0c707445407cfe3616cad9fdbc84d3e
원민관 <mkwan5741@gmail.com>
2025-03-19 20:23:42 +0900

    github connection check2

--- server/src/modules/auth/naver/naver.auth.repository.ts
+++ server/src/modules/auth/naver/naver.auth.repository.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { NaverUser } from './naver.auth.entity';
 
-// github 확인용
+// github 확인용2
 
 @Injectable()
 export class NaverAuthRepository {
