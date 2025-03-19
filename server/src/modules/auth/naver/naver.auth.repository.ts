b0a7f811aebe4eb41b3b07a750b076eddb620052
원민관 <mkwan5741@gmail.com>
2025-03-19 20:54:32 +0900

    테스트 용 변경

--- server/src/modules/auth/naver/naver.auth.repository.ts
+++ server/src/modules/auth/naver/naver.auth.repository.ts
@@ -3,8 +3,6 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { NaverUser } from './naver.auth.entity';
 
-// github 연결213
-
 @Injectable()
 export class NaverAuthRepository {
   constructor(
@@ -14,6 +12,7 @@ export class NaverAuthRepository {
 
   // 1. 사용자 정보가 존재하는지 확인하고, 없으면 생성
   async findUser(user: { id: string }): Promise<NaverUser> {
+    console.log('깃허브 테스트용:', user);
     const existingUser = await this.naverUserRepository.findOneBy({
       id: user?.id,
     });
