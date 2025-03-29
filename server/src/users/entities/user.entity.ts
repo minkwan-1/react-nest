211e204a35e4fbfd4a9b06f8a90ea3fa29bf507a
원민관 <mkwan5741@gmail.com>
2025-03-29 17:46:04 +0900

    feat: create user entity

--- server/src/users/entities/user.entity.ts
+++ server/src/users/entities/user.entity.ts
@@ -1,1 +1,28 @@
-export class User {}
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  phoneNumber: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
