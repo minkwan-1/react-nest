f548995fd8cc9e606385a82c3411bc4edaca2f1e
원민관 <mkwan5741@gmail.com>
2025-03-29 17:47:39 +0900

    feat: create-user.dto.ts

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  phoneNumber: string;
+}
