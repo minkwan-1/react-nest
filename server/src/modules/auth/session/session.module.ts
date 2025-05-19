d8a0ee3e5ff77545da908b9d4306da4a68b634c8
원민관 <mkwan5741@gmail.com>
2025-05-19 15:57:07 +0900

    Session Module 설정

--- server/src/modules/auth/session/session.module.ts
+++ server/src/modules/auth/session/session.module.ts
@@ -2,14 +2,17 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
 import { UserSession } from './session.entity';
+import { SessionController } from './session.controller';
+import { SessionService } from './session.service';
+import { SessionRepository } from './session.repository';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserSession]),
     PassportModule.register({ session: true }),
   ],
-  controllers: [],
-  providers: [],
-  exports: [],
+  controllers: [SessionController],
+  providers: [SessionService, SessionRepository],
+  exports: [SessionService],
 })
 export class SessionModule {}
