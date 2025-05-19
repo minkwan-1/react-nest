2e1f1038beee95d618d5af86b86271695f251e4c
원민관 <mkwan5741@gmail.com>
2025-05-19 16:09:15 +0900

    save session - repository

--- server/src/modules/auth/session/session.repository.ts
+++ server/src/modules/auth/session/session.repository.ts
@@ -7,17 +7,26 @@ export class SessionRepository extends Repository<UserSession> {
   constructor(private dataSource: DataSource) {
     super(UserSession, dataSource.createEntityManager());
   }
-  // 향후 동시 트랜잭션 처리 시 적용할 것
-  //   async transactionSave(
-  //     transactionManager: EntityManager,
-  //     userSession: UserSession,
-  //   ): Promise<UserSession> {
-  //     return await transactionManager.save(userSession);
-  //   }
 
-  // 1. save session
+  // 1. save session(로그인)
+  saveSession(
+    sessionId: string,
+    userId: string,
+    provider: string,
+    createdAt: Date,
+    expiresAt: Date,
+    data?: any,
+  ): Promise<UserSession> {
+    const session = new UserSession();
+    session.sessionId = sessionId;
+    session.userId = userId;
+    session.provider = provider;
+    session.createdAt = createdAt;
+    session.expiresAt = expiresAt;
+    session.data = data;
+    return this.save(session);
+  }
+  // 2. find session(유저 정보 조회)
 
-  // 2. find session
-
-  // 3. clear session
+  // 3. clear session(로그아웃)
 }
