eb9a3b3b1cdd1d208454844742bf15da314b5af2
원민관 <mkwan5741@gmail.com>
2025-05-26 16:28:40 +0900

    frontend, 필요없는 페이지 삭제

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,16 +6,11 @@ import {
   NotFoundPage,
   TermsAndPrivacyPage,
   QuestionEditPage,
-  // TestPage,
   QuestionDetailPage,
   RedirectPage,
   LandingPage,
-  CommunityPage,
-  TagPage,
-  AskAIPage,
   SignUpPage,
   SignInPage,
-  TestPage,
 } from "./pages";
 
 const App = () => {
@@ -23,23 +18,15 @@ const App = () => {
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/home" element={<HomePage />} />
-      {/* <Route path="/question" element={<QuestionListPage />} /> */}
       <Route path="/questions/:id" element={<QuestionDetailPage />} />
       <Route path="/my" element={<MyPage />} />
-      {/* <Route path="/auth" element={<AuthPage />} /> */}
       <Route path="/phone" element={<PhoneVerificationPage />} />
       <Route path="/privacy" element={<TermsAndPrivacyPage />} />
       <Route path="/edit" element={<QuestionEditPage />} />
       <Route path="*" element={<NotFoundPage />} />
-      {/* <Route path="/test" element={<TestPage />} /> */}
       <Route path="/redirect" element={<RedirectPage />} />
-      <Route path="/community" element={<CommunityPage />} />
-      {/* <Route path="/collection" element={<CollectionPage />} /> */}
-      <Route path="/tag" element={<TagPage />} />
-      <Route path="/ask-ai" element={<AskAIPage />} />
       <Route path="/sign-up" element={<SignUpPage />} />
       <Route path="/sign-in" element={<SignInPage />} />
-      <Route path="/test" element={<TestPage />} />
     </Routes>
   );
 };
--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,41 +1,25 @@
 import HomePage from "./HomePage";
-import QuestionListPage from "./QuestionListPage";
 import MyPage from "./MyPage";
-import AuthPage from "./AuthPage";
 import PhoneVerificationPage from "./PhoneVerificationPage";
 import NotFoundPage from "./NotFoundPage";
 import TermsAndPrivacyPage from "./TermsAndPrivacyPage";
 import QuestionEditPage from "./QuestionEditPage";
 import QuestionDetailPage from "./QuestionDetailPage";
 import LandingPage from "./LandingPage";
-// import TestPage from "./TestPage";
 import RedirectPage from "./RedirectPage";
-import CommunityPage from "./CommunityPage";
-import CollectionPage from "./CollectionPage";
-import TagPage from "./TagPage";
-import AskAIPage from "./AskAIPage";
 import SignUpPage from "./SignUpPage";
 import SignInPage from "./SignInPage";
-import TestPage from "./TestPage";
 
 export {
   HomePage,
-  QuestionListPage,
   MyPage,
-  AuthPage,
   PhoneVerificationPage,
   NotFoundPage,
   TermsAndPrivacyPage,
   QuestionEditPage,
   QuestionDetailPage,
   LandingPage,
-  // TestPage,
   RedirectPage,
-  CommunityPage,
-  CollectionPage,
-  TagPage,
-  AskAIPage,
   SignUpPage,
   SignInPage,
-  TestPage,
 };
