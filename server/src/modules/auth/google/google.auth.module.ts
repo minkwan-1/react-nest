6853d40560dba8be53cc83e446755798351d1a3f
원민관 <mkwan5741@gmail.com>
2025-05-26 15:15:30 +0900

    google module 수정

--- server/src/modules/auth/google/google.auth.module.ts
+++ server/src/modules/auth/google/google.auth.module.ts
@@ -2,31 +2,24 @@ import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
 
-// 1. 엔티티 및 리포지토리
+// 엔티티 및 리포지토리
 import { GoogleUser } from './google.auth.entity';
 import { GoogleAuthRepository } from './google.auth.repository';
 
-// 2. 서비스 및 컨트롤러
+// 서비스 및 컨트롤러
 import { GoogleAuthService } from './google.auth.service';
 import { GoogleAuthController } from './google.auth.controller';
 
-// 3. 의존 모듈
+// 의존 모듈
 import { UsersModule } from 'src/modules/users/users.module';
-import { AuthModule } from '../auth.module';
 import { SessionModule } from '../session/session.module';
+import { AuthModule } from '../auth.module';
 
 @Module({
   imports: [
-    // GoogleUser 엔티티를 TypeORM에 등록
     TypeOrmModule.forFeature([GoogleUser]),
-
-    // 세션 기반 Passport 인증 사용
     PassportModule.register({ session: true }),
-
-    // 순환 참조 해결을 위한 forwardRef
     forwardRef(() => AuthModule),
-
-    // 유저 정보 접근용 모듈
     UsersModule,
     SessionModule,
   ],
