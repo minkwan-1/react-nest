7adfcd0038bb2068ce58f74bd30dc5875da11070
원민관 <mkwan5741@gmail.com>
2025-05-26 15:37:18 +0900

    naver module 정리

--- server/src/modules/auth/naver/naver.auth.module.ts
+++ server/src/modules/auth/naver/naver.auth.module.ts
@@ -2,31 +2,27 @@ import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
 
-// 1. 엔티티 및 리포지토리
+// 엔티티 및 리포지토리
 import { NaverUser } from './naver.auth.entity';
 import { NaverAuthRepository } from './naver.auth.repository';
 
-// 2. 서비스 및 컨트롤러
+// 서비스 및 컨트롤러
 import { NaverAuthService } from './naver.auth.service';
 import { NaverAuthController } from './naver.auth.controller';
 
-// 3. 의존 모듈
+// 의존 모듈
 import { AuthModule } from '../auth.module';
 import { UsersModule } from 'src/modules/users/users.module';
 import { SessionModule } from '../session/session.module';
 
 @Module({
   imports: [
-    // NaverUser 엔티티 등록
     TypeOrmModule.forFeature([NaverUser]),
 
-    // 세션 기반 Passport 사용
     PassportModule.register({ session: true }),
 
-    // AuthModule과의 순환 의존성 해결
     forwardRef(() => AuthModule),
 
-    // 유저 정보 접근을 위한 UsersModule
     UsersModule,
     SessionModule,
   ],
