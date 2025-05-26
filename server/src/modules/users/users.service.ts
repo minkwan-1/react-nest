6dcb0c05b5dcddd47401d17721573b748df474e0
원민관 <mkwan5741@gmail.com>
2025-05-26 16:16:43 +0900

    user service 정리

--- server/src/modules/users/users.service.ts
+++ server/src/modules/users/users.service.ts
@@ -5,20 +5,19 @@ import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  // 1. 생성자에서 UsersRepository 주입
   constructor(private readonly usersRepository: UsersRepository) {}
 
-  // 2. 유저 생성
+  // [1] 유저 생성
   async create(createUserDto: CreateUserDto): Promise<User> {
     return this.usersRepository.create(createUserDto);
   }
 
-  // 3. 이메일로 유저 조회
+  // [2] 이메일로 유저 조회
   async findByEmail(email: string): Promise<User | null> {
     return this.usersRepository.findOneByEmail(email);
   }
 
-  // 4. ID로 유저 조회
+  // [3] ID로 유저 조회
   async findById(id: string): Promise<User | null> {
     return this.usersRepository.findById(id);
   }
