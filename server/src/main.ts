bd8820c7cfe60cfc2b62959d86d43d69d211e4b9
원민관 <mkwan5741@gmail.com>
2025-05-26 15:00:59 +0900

    backend, main.ts 가독성 확보

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import * as cookieParser from 'cookie-parser';
 import * as session from 'express-session';
 import * as passport from 'passport';
-import * as cookieParser from 'cookie-parser';
 
 // .env 파일 로드
 dotenv.config();
@@ -11,49 +11,49 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // CORS 설정
+  const { CORS_ORIGIN, FRONTEND_URL, SESSION_SECRET, CSP_SCRIPT_SRC, PORT } =
+    process.env;
+
+  // CORS
   app.enableCors({
-    origin:
-      process.env.CORS_ORIGIN ||
-      process.env.FRONTEND_URL ||
-      'http://localhost:5173',
+    origin: CORS_ORIGIN || FRONTEND_URL || 'http://localhost:5173',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
 
-  // cookie-parser 적용
+  // cookie
   app.use(cookieParser());
 
-  // session 설정
+  // session
   app.use(
     session({
-      secret: process.env.SESSION_SECRET,
+      secret: SESSION_SECRET,
       resave: false,
       saveUninitialized: false,
       cookie: {
-        maxAge: 60000 * 60 * 24, // 24시간
+        maxAge: 1000 * 60 * 60 * 24,
         httpOnly: false,
         secure: false,
       },
     }),
   );
 
-  // passport 초기화 및 세션 적용
+  // passport
   app.use(passport.initialize());
-
   app.use(passport.session());
 
-  // CSP(Content-Security-Policy) 설정
+  // CSP
   app.use((req, res, next) => {
     res.setHeader(
       'Content-Security-Policy',
-      `script-src 'self' ${process.env.CSP_SCRIPT_SRC || ''} 'unsafe-eval';`,
+      `script-src 'self' ${CSP_SCRIPT_SRC || ''} 'unsafe-eval';`,
     );
     next();
   });
 
-  await app.listen(process.env.PORT || 3000);
+  // PORT
+  await app.listen(PORT || 3000);
 }
 
 bootstrap();
