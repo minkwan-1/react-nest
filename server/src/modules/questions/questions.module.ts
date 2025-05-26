05405105990d8a234a0baee67c6ddca3cde3a28c
원민관 <mkwan5741@gmail.com>
2025-05-26 16:12:00 +0900

    question module 정리

--- server/src/modules/questions/questions.module.ts
+++ server/src/modules/questions/questions.module.ts
@@ -1,23 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-// 1. 질문 관련 엔티티
+// 엔티티
 import { Question } from './questions.entity';
 
-// 2. 질문 관련 서비스 및 컨트롤러
+// 서비스 및 컨트롤러
 import { QuestionsService } from './questions.service';
 import { QuestionsController } from './questions.controller';
 
 import { UsersModule } from 'src/modules/users/users.module';
 
 @Module({
-  // TypeORM에 Question 엔티티 등록
   imports: [TypeOrmModule.forFeature([Question]), UsersModule],
 
-  // REST API 라우팅 처리
   controllers: [QuestionsController],
 
-  // 비즈니스 로직 처리
   providers: [QuestionsService],
 })
 export class QuestionsModule {}
