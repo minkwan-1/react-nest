c3c21dc48a119174e78f01bfd0dd813638125013
원민관 <mkwan5741@gmail.com>
2025-06-14 19:33:08 +0900

    myInfo 등록을 위한 서비스 로직 작성

--- server/src/modules/myInfo/myInfo.service.ts
+++ server/src/modules/myInfo/myInfo.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MyInfo } from './myInfo.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class MyInfoService {}
+export class MyInfoService {
+  constructor(
+    @InjectRepository(MyInfo)
+    private readonly myInfoRepository: Repository<MyInfo>,
+  ) {}
+
+  // myInfo 등록 처리
+  async upsert(
+    userId: string,
+    job: string,
+    interests: string[],
+    socialLinks: string[],
+  ): Promise<void> {
+    const existing = await this.myInfoRepository.findOne({ where: { userId } });
+    if (existing) {
+      existing.job = job;
+      existing.interests = interests;
+      existing.socialLinks = socialLinks;
+
+      await this.myInfoRepository.save(existing);
+    } else {
+      const newMyInfo = { job, interests, socialLinks };
+
+      await this.myInfoRepository.save({ userId, ...newMyInfo });
+    }
+  }
+}
