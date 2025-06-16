10c5ecc60e5d0c10ce231c490be811f7f18862c0
원민관 <mkwan5741@gmail.com>
2025-06-16 16:17:50 +0900

    프로필 정보 불러오기, hook으로 분리

---
+++ client/src/components/my/hooks/useFetchMyInfo.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface MyInfoType {
+  id: string;
+  userId: string;
+  job: string;
+  interests: string[];
+  socialLinks: string[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+const useFetchMyInfo = (userId: string | undefined) => {
+  const [myInfo, setMyInfo] = useState<MyInfoType | null>(null);
+
+  useEffect(() => {
+    const fetchMyInfo = async () => {
+      if (!userId) return;
+
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/my-info?id=${userId}`
+        );
+        setMyInfo(response.data?.myInfo);
+      } catch (err) {
+        console.error("프로필 정보 불러오기 실패: ", err);
+      }
+    };
+
+    fetchMyInfo();
+  }, [userId]);
+
+  return myInfo;
+};
+
+export default useFetchMyInfo;
