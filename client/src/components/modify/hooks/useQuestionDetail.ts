2c35e2eb27c17c9febd613b0d92ba8da0ab9a5e8
원민관 <mkwan5741@gmail.com>
2025-07-21 15:11:46 +0900

    refactor: useQuery를 적용하여 질문 상세 조회 훅 리팩토링

--- client/src/components/modify/hooks/useQuestionDetail.ts
+++ client/src/components/modify/hooks/useQuestionDetail.ts
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
-import { API_URL } from "@api/axiosConfig";
+import { useQuery } from "@tanstack/react-query";
+import { fetchQuestionDetail } from "../api/fetchQuestionDetail";
+
 export interface Question {
   id: number;
   title: string;
@@ -11,29 +12,13 @@ export interface Question {
 }
 
 export const useQuestionDetail = (questionId?: string) => {
-  const [question, setQuestion] = useState<Question | null>(null);
-  const [loading, setLoading] = useState(true);
+  const { data: question, isPending: loading } = useQuery<Question>({
+    queryKey: ["questions", questionId],
 
-  useEffect(() => {
-    if (!questionId) return;
+    queryFn: () => fetchQuestionDetail(questionId!),
 
-    const fetchQuestion = async () => {
-      try {
-        setLoading(true);
-        const res = await fetch(`${API_URL}questions/${questionId}`);
-        if (!res.ok) throw new Error("질문을 불러올 수 없습니다.");
-        const data = await res.json();
-        setQuestion(data);
-      } catch (err) {
-        console.error(err);
-        alert("질문을 불러오는데 실패했습니다.");
-      } finally {
-        setLoading(false);
-      }
-    };
+    enabled: !!questionId,
+  });
 
-    fetchQuestion();
-  }, [questionId]);
-
-  return { question, loading };
+  return { question: question ?? null, loading };
 };
