dbbb24d88f6808cf1cd39520b59a404a754f9f74
원민관 <mkwan5741@gmail.com>
2025-07-29 14:08:35 +0900

    feat: snackbar atom에 type 속성 추가

--- client/src/atom/snackbarAtom.ts
+++ client/src/atom/snackbarAtom.ts
@@ -1,11 +1,15 @@
 import { atom } from "jotai";
 
+export type SnackbarType = "success" | "error" | "info";
+
 interface snackbarAtomState {
   isOpen: boolean;
+  type: SnackbarType;
   message: string;
 }
 
 export const snackbarAtom = atom<snackbarAtomState>({
   isOpen: false,
+  type: "info",
   message: "",
 });
