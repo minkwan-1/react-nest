ef5258140c2ceb5b9145bdbba48d832a8d6f36c4
원민관 <mkwan5741@gmail.com>
2025-09-02 16:25:35 +0900

    ec2 테스트를 위해 비활성화

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,19 +1,17 @@
-// src/app.module.ts
-
 import { Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { TypeOrmConfigService } from './configs/typeorm.config'; // 수정한 설정 서비스를 import
-import { AuthModule } from './modules/auth/auth.module';
-import { PhoneVerificationModule } from './modules/phone/phone-verification.module';
-import { UsersModule } from './modules/users/users.module';
-import { QuestionsModule } from './modules/questions/questions.module';
-import { AiModule } from './modules/ai/ai.module';
-import { SelfIntroModule } from './modules/self-intro/self-intro.module';
-import { MyInfoModule } from './modules/myInfo/myInfo.module';
-import { AnswerModule } from './modules/answer/answer.module';
-import { NewsModule } from './modules/news/news.module';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { TypeOrmConfigService } from './configs/typeorm.config';
+// import { AuthModule } from './modules/auth/auth.module';
+// import { PhoneVerificationModule } from './modules/phone/phone-verification.module';
+// import { UsersModule } from './modules/users/users.module';
+// import { QuestionsModule } from './modules/questions/questions.module';
+// import { AiModule } from './modules/ai/ai.module';
+// import { SelfIntroModule } from './modules/self-intro/self-intro.module';
+// import { MyInfoModule } from './modules/myInfo/myInfo.module';
+// import { AnswerModule } from './modules/answer/answer.module';
+// import { NewsModule } from './modules/news/news.module';
 import { CatchEverythingFilter } from './filters/catch-everything.filter';
 import { HttpExceptionFilter } from './filters/http-exceptions.filter';
 
@@ -21,20 +19,22 @@ import { HttpExceptionFilter } from './filters/http-exceptions.filter';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
 
+    /*
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useClass: TypeOrmConfigService,
     }),
+    */
 
-    AuthModule,
-    PhoneVerificationModule,
-    UsersModule,
-    QuestionsModule,
-    AiModule,
-    SelfIntroModule,
-    MyInfoModule,
-    AnswerModule,
-    NewsModule,
+    // AuthModule,
+    // PhoneVerificationModule,
+    // UsersModule,
+    // QuestionsModule,
+    // AiModule,
+    // SelfIntroModule,
+    // MyInfoModule,
+    // AnswerModule,
+    // NewsModule,
   ],
   providers: [
     { provide: APP_FILTER, useClass: CatchEverythingFilter },
