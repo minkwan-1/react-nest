38971e0fbca4f5dcde60a5b247c662e7e7b39526
원민관 <mkwan5741@gmail.com>
2025-09-07 16:20:34 +0900

    app module 주석 해제

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { TypeOrmConfigService } from './configs/typeorm.config';
-// import { AuthModule } from './modules/auth/auth.module';
-// import { PhoneVerificationModule } from './modules/phone/phone-verification.module';
-// import { UsersModule } from './modules/users/users.module';
-// import { QuestionsModule } from './modules/questions/questions.module';
-// import { AiModule } from './modules/ai/ai.module';
-// import { SelfIntroModule } from './modules/self-intro/self-intro.module';
-// import { MyInfoModule } from './modules/myInfo/myInfo.module';
-// import { AnswerModule } from './modules/answer/answer.module';
-// import { NewsModule } from './modules/news/news.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './configs/typeorm.config';
+import { AuthModule } from './modules/auth/auth.module';
+import { PhoneVerificationModule } from './modules/phone/phone-verification.module';
+import { UsersModule } from './modules/users/users.module';
+import { QuestionsModule } from './modules/questions/questions.module';
+import { AiModule } from './modules/ai/ai.module';
+import { SelfIntroModule } from './modules/self-intro/self-intro.module';
+import { MyInfoModule } from './modules/myInfo/myInfo.module';
+import { AnswerModule } from './modules/answer/answer.module';
+import { NewsModule } from './modules/news/news.module';
 import { CatchEverythingFilter } from './filters/catch-everything.filter';
 import { HttpExceptionFilter } from './filters/http-exceptions.filter';
 
@@ -19,22 +19,20 @@ import { HttpExceptionFilter } from './filters/http-exceptions.filter';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
 
-    /*
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useClass: TypeOrmConfigService,
     }),
-    */
 
-    // AuthModule,
-    // PhoneVerificationModule,
-    // UsersModule,
-    // QuestionsModule,
-    // AiModule,
-    // SelfIntroModule,
-    // MyInfoModule,
-    // AnswerModule,
-    // NewsModule,
+    AuthModule,
+    PhoneVerificationModule,
+    UsersModule,
+    QuestionsModule,
+    AiModule,
+    SelfIntroModule,
+    MyInfoModule,
+    AnswerModule,
+    NewsModule,
   ],
   providers: [
     { provide: APP_FILTER, useClass: CatchEverythingFilter },
